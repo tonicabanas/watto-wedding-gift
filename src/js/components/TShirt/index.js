a55647ac467f49a8e232199065ed05d54b5c61c6
Antonio Cabanas <acabanas@mrmilu.com>
2018-05-02 19:59:21 +0200

    Add palms and favicon

--- src/js/components/TShirt/index.js
+++ src/js/components/TShirt/index.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './style.scss';
 
 export default class TShirt extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    number: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  getNumber() {
+    return Math.sqrt(this.props.number / 5);
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value,
+    });
+  }
+
+  renderCorrect() {
+    return (parseInt(this.getNumber()) === parseInt(this.state.inputValue) ? <div className='TShirt-palm'/> : null);
+  }
+
   render() {
     return (
       <div className='TShirt'>
         <div className='TShirt-img'>
           <div className='TShirt-name'>{ this.props.name }</div>
-          <div className='TShirt-number'><input type='number no-spinners'/></div>
+          <div className='TShirt-number'><input value={ this.state.inputValue }
+                                                onChange={ evt => this.updateInputValue(evt) }
+                                                type='number no-spinners'/></div>
+          { this.renderCorrect() }
         </div>
       </div>
     );
--- src/js/views/Dashboard/index.js
+++ src/js/views/Dashboard/index.js
@@ -5,17 +5,18 @@ import './style.scss';
 
 const Dashboard = () => (
   <div className='Dashboard'>
-    <TShirt name={ 'Tiki' }/>
-    <TShirt name={ 'Taka' }/>
-    <TShirt name={ 'Machicambo' }/>
-    <TShirt name={ 'Licor' }/>
-    <TShirt name={ 'Agui.Haz' }/>
-    <TShirt name={ 'Max Power' }/>
-    <TShirt name={ 'Jorginho' }/>
-    <TShirt name={ 'Bohada' }/>
-    <TShirt name={ 'Uge' }/>
-    <TShirt name={ 'Todassss' }/>
-    <TShirt name={ 'Kike' }/>
+    <TShirt name={ 'Tiki' } number={ 180 }/>
+    <TShirt name={ 'Taka' } number={ 405 }/>
+    <TShirt name={ 'Machicambo' } number={ 845 }/>
+    <TShirt name={ 'Licor' } number={ 9245 }/>
+    <TShirt name={ 'Agui.Haz' } number={ 980 }/>
+    <TShirt name={ 'Max Power' } number={ 245 }/>
+    <TShirt name={ 'Jorginho' } number={ 500 }/>
+    <TShirt name={ 'Bohada' } number={ 605 }/>
+    <TShirt name={ 'Uge' } number={ 80 }/>
+    <TShirt name={ 'Todassss' } number={ 2205 }/>
+    <TShirt name={ 'Kike' } number={ 2645 }/>
+    <TShirt name={ 'Xavi' } number={ 0 }/>
   </div>
 );
 
